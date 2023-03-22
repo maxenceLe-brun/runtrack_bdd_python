b82d701b279c4973a9ac9babe9888e942c8ebcb1
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-03-22 15:48:00 +0100

    Create job01.py

---
+++ jour02/job01.py
@@ -0,0 +1,16 @@
+import mysql
+
+connection_params = {
+    'host': "localhost",
+    'user': "root",
+    'password': "root",
+    'database': "laplateforme",
+}
+
+with mysql.connector.connect(**connection_params) as db :
+    db.autocommit = True
+    with db.cursor() as c:
+        c.execute("SELECT * FROM etudiant")
+        for i in c.fetchall():
+            print(i)
+        pass
