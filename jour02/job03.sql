dc9e45379cbd80f4fe29200a298677d8da6b4425
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-03-22 16:16:00 +0100

    Create job03.sql

---
+++ jour02/job03.sql
@@ -0,0 +1,18 @@
+insert into etage (name, numero, superficie)
+    -> value ("RDC", 0, 500);
+insert into etage (name, numero, superficie)
+    -> value ("R+1", 1, 500);
+    
+
+insert into salles (name, id_etage, capacite)
+    -> value ("Lounge", 1, 100);
+insert into salles (name, id_etage, capacite)
+    -> value ("Studio son", 1, 5);
+insert into salles (name, id_etage, capacite)
+    -> value ("Broadcasting", 2, 50);
+insert into salles (name, id_etage, capacite)
+    -> value ("Bocal pPeda", 2, 4);
+insert into salles (name, id_etage, capacite)
+    -> value ("Coworking", 2, 80);
+insert into salles (name, id_etage, capacite)
+    -> value ("Studio Video", 2, 5);
